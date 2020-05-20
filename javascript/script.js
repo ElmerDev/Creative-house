8ff4abd9a3dd15339111d666b598e0cb6d626b44
Elmer Raimundo dos Santos <eelmer226@gmail.com>
2020-05-20 13:16:33 -0300

    Arquivos

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+function onOff () {
+    document
+        .querySelector('#modal')
+        .classList
+        .toggle('hide')
+    
+    document
+        .querySelector('body')
+        .classList
+        .toggle('hideScroll')
+
+    document
+        .querySelector('#modal')
+        .classList
+        .toggle('addScroll')
+}
